dfbab7362add3c8d07563d8e3a9b7422791822a3
neptunus alien <neptunusalien@outlook.com>
2022-09-25 19:01:12 +0700

    update

--- Laporan penjualan.Designer.vb
+++ Laporan penjualan.Designer.vb
@@ -23,15 +23,16 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.LVpenjualan = New System.Windows.Forms.ListView()
-        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader6 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Ttl_pndptn = New System.Windows.Forms.TextBox()
         Me.GroupBox1.SuspendLayout()
-
         Me.SuspendLayout()
         '
         'Label1
@@ -44,91 +45,102 @@ Partial Class Form1
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "LAPORAN PENJUALAN"
         '
-        'LVpenjualan
-        '
-        Me.LVpenjualan.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
-        Me.LVpenjualan.GridLines = True
-        Me.LVpenjualan.HideSelection = False
-        Me.LVpenjualan.Location = New System.Drawing.Point(457, 96)
-        Me.LVpenjualan.Name = "LVpenjualan"
-        Me.LVpenjualan.Size = New System.Drawing.Size(331, 267)
-        Me.LVpenjualan.TabIndex = 1
-        Me.LVpenjualan.UseCompatibleStateImageBehavior = False
-        Me.LVpenjualan.View = System.Windows.Forms.View.Details
-        '
-        'ColumnHeader1
-        '
-        Me.ColumnHeader1.Text = "Menu"
-        Me.ColumnHeader1.Width = 150
-        '
-        'ColumnHeader2
-        '
-        Me.ColumnHeader2.Text = "Total Penjualan"
-        Me.ColumnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
-        Me.ColumnHeader2.Width = 175
-        '
         'GroupBox1
         '
         Me.GroupBox1.Controls.Add(Me.ListView1)
         Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.GroupBox1.Location = New System.Drawing.Point(13, 58)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(383, 331)
+        Me.GroupBox1.Size = New System.Drawing.Size(775, 331)
         Me.GroupBox1.TabIndex = 2
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Menu Terlaris"
         '
         'ListView1
         '
-        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader6})
         Me.ListView1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ListView1.GridLines = True
         Me.ListView1.HideSelection = False
-        Me.ListView1.Location = New System.Drawing.Point(0, 38)
+        Me.ListView1.Location = New System.Drawing.Point(6, 47)
         Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(356, 267)
+        Me.ListView1.Size = New System.Drawing.Size(763, 267)
         Me.ListView1.TabIndex = 3
         Me.ListView1.UseCompatibleStateImageBehavior = False
         Me.ListView1.View = System.Windows.Forms.View.Details
         '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "NO"
+        Me.ColumnHeader1.Width = 53
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Menu"
+        Me.ColumnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ColumnHeader2.Width = 198
+        '
         'ColumnHeader3
         '
-        Me.ColumnHeader3.Text = "Menu"
-        Me.ColumnHeader3.Width = 150
+        Me.ColumnHeader3.Text = "Total Porsi"
+        Me.ColumnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ColumnHeader3.Width = 128
         '
         'ColumnHeader4
         '
-        Me.ColumnHeader4.Text = "Total Porsi"
+        Me.ColumnHeader4.Text = "Harga"
         Me.ColumnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
-        Me.ColumnHeader4.Width = 200
+        Me.ColumnHeader4.Width = 149
         '
-
+        'ColumnHeader6
+        '
+        Me.ColumnHeader6.Text = "Total Penjualan"
+        Me.ColumnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ColumnHeader6.Width = 192
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(31, 409)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(304, 32)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "TOTAL PENDAPATAN"
+        '
+        'Ttl_pndptn
+        '
+        Me.Ttl_pndptn.Location = New System.Drawing.Point(379, 409)
+        Me.Ttl_pndptn.Multiline = True
+        Me.Ttl_pndptn.Name = "Ttl_pndptn"
+        Me.Ttl_pndptn.Size = New System.Drawing.Size(357, 33)
+        Me.Ttl_pndptn.TabIndex = 4
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(800, 467)
+        Me.Controls.Add(Me.Ttl_pndptn)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.LVpenjualan)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.GroupBox1.ResumeLayout(False)
-        Me.Controls.Add(Me.Label1)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Label1 As Label
-    Friend WithEvents LVpenjualan As ListView
-    Friend WithEvents ColumnHeader1 As ColumnHeader
-    Friend WithEvents ColumnHeader2 As ColumnHeader
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents ListView1 As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
     Friend WithEvents ColumnHeader3 As ColumnHeader
     Friend WithEvents ColumnHeader4 As ColumnHeader
-
+    Friend WithEvents ColumnHeader6 As ColumnHeader
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Ttl_pndptn As TextBox
 End Class
--- Menu.Designer.vb
+++ Menu.Designer.vb
@@ -37,9 +37,10 @@ Partial Class Menu
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(200, 45)
+        Me.Label3.Location = New System.Drawing.Point(300, 69)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(251, 25)
+        Me.Label3.Size = New System.Drawing.Size(382, 37)
         Me.Label3.TabIndex = 6
         Me.Label3.Text = "MENU PADANG MURAH"
         '
@@ -47,9 +48,10 @@ Partial Class Menu
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Adobe Caslon Pro", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(56, 125)
+        Me.Label1.Location = New System.Drawing.Point(84, 192)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(44, 27)
+        Me.Label1.Size = New System.Drawing.Size(66, 41)
         Me.Label1.TabIndex = 7
         Me.Label1.Text = "Lauk"
         '
@@ -57,9 +59,10 @@ Partial Class Menu
         '
         Me.CheckBox1.AutoSize = True
         Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox1.Location = New System.Drawing.Point(123, 124)
+        Me.CheckBox1.Location = New System.Drawing.Point(184, 191)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(63, 24)
+        Me.CheckBox1.Size = New System.Drawing.Size(96, 33)
         Me.CheckBox1.TabIndex = 9
         Me.CheckBox1.Text = "Telur"
         Me.CheckBox1.UseVisualStyleBackColor = True
@@ -68,9 +71,10 @@ Partial Class Menu
         '
         Me.CheckBox2.AutoSize = True
         Me.CheckBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox2.Location = New System.Drawing.Point(123, 169)
+        Me.CheckBox2.Location = New System.Drawing.Point(184, 260)
+        Me.CheckBox2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(68, 24)
+        Me.CheckBox2.Size = New System.Drawing.Size(98, 33)
         Me.CheckBox2.TabIndex = 10
         Me.CheckBox2.Text = "Ayam"
         Me.CheckBox2.UseVisualStyleBackColor = True
@@ -79,9 +83,10 @@ Partial Class Menu
         '
         Me.CheckBox3.AutoSize = True
         Me.CheckBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox3.Location = New System.Drawing.Point(123, 210)
+        Me.CheckBox3.Location = New System.Drawing.Point(184, 323)
+        Me.CheckBox3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.CheckBox3.Name = "CheckBox3"
-        Me.CheckBox3.Size = New System.Drawing.Size(94, 24)
+        Me.CheckBox3.Size = New System.Drawing.Size(137, 33)
         Me.CheckBox3.TabIndex = 11
         Me.CheckBox3.Text = "Rendang"
         Me.CheckBox3.UseVisualStyleBackColor = True
@@ -90,9 +95,10 @@ Partial Class Menu
         '
         Me.CheckBox4.AutoSize = True
         Me.CheckBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox4.Location = New System.Drawing.Point(498, 124)
+        Me.CheckBox4.Location = New System.Drawing.Point(747, 191)
+        Me.CheckBox4.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.CheckBox4.Name = "CheckBox4"
-        Me.CheckBox4.Size = New System.Drawing.Size(112, 24)
+        Me.CheckBox4.Size = New System.Drawing.Size(164, 33)
         Me.CheckBox4.TabIndex = 12
         Me.CheckBox4.Text = "Teh Hangat"
         Me.CheckBox4.UseVisualStyleBackColor = True
@@ -101,9 +107,10 @@ Partial Class Menu
         '
         Me.CheckBox5.AutoSize = True
         Me.CheckBox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox5.Location = New System.Drawing.Point(498, 169)
+        Me.CheckBox5.Location = New System.Drawing.Point(747, 260)
+        Me.CheckBox5.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.CheckBox5.Name = "CheckBox5"
-        Me.CheckBox5.Size = New System.Drawing.Size(78, 24)
+        Me.CheckBox5.Size = New System.Drawing.Size(116, 33)
         Me.CheckBox5.TabIndex = 13
         Me.CheckBox5.Text = "Es Teh"
         Me.CheckBox5.UseVisualStyleBackColor = True
@@ -111,9 +118,10 @@ Partial Class Menu
         'Button1
         '
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(283, 264)
+        Me.Button1.Location = New System.Drawing.Point(424, 406)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(95, 28)
+        Me.Button1.Size = New System.Drawing.Size(142, 43)
         Me.Button1.TabIndex = 14
         Me.Button1.Text = "TOTAL"
         Me.Button1.UseVisualStyleBackColor = True
@@ -122,17 +130,18 @@ Partial Class Menu
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Adobe Caslon Pro", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(405, 124)
+        Me.Label2.Location = New System.Drawing.Point(608, 191)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(75, 27)
+        Me.Label2.Size = New System.Drawing.Size(113, 41)
         Me.Label2.TabIndex = 15
         Me.Label2.Text = "Minuman"
         '
         'Menu
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(650, 338)
+        Me.ClientSize = New System.Drawing.Size(975, 520)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.CheckBox5)
@@ -142,6 +151,7 @@ Partial Class Menu
         Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Label3)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Menu"
         Me.Text = "Menu"
         Me.ResumeLayout(False)
